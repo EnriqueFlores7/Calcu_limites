7466b99aa83e29e7e60e337259ee945a5bb317aa
Enrique Flores Ortiz <floresnenrique1@gmail.com>
2023-10-12 21:36:13 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.set('view engine','hbs');
+hbs.registerPartial(__dirname + '/views/partials', ()=>{});
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+//Paginas en uso
+app.get('/calcu', (req,res)=>{
+    res.render('UI_limites');
+});
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+app.listen(port,()=>{
+    console.log('Serviodor corriendo en el puerto: ',port);
+});
